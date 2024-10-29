884ff79ed9d1585a3a9fe4e210574077481d1b9c
oussamaaouam1 <oussama.aouam@gmail.com>
2024-10-29 20:46:56 +0100

    test versel

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import AddTransaction from './components/AddTransaction'
 import Balance from './components/Balance'
 import Header from './components/Header'
-import IncomeExpenses from './components/incomeExpenses'
+import IncomeExpenses from './components/IncomeExpenses'
 import TransactionList from './components/TransactionList'
 import { GlobalProvider } from './context/GlobalState'
 
